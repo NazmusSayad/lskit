597e2a35f279424acbc4f43389b40d13d1e49c71
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-06-11 23:10:00 +0000

    M package.json  M src/config.ts ?? README.md

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
-import { Options } from "./types";
+import { Options } from './types'
 
 export const defaultConfig: Options = {
-  excludeFolders: ['node_modules'],
-  relative: true,
+  excludeFolders: ['node_modules', '.git', '.cache'],
   minSize: 0,
   maxSize: Infinity,
+  relative: false,
 }
